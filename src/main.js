75c61428a34489351cd2be6012a8d1f6cb5a96ed
Ramil <sattarovramil1@gmail.com>
2025-05-25 19:30:17 +0300

    auto slide change and change by pressing arrows

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 let slideIndex = 1;
+let autoSlideInterval
 showSlides(slideIndex);
 
 function plusSlides(n){
@@ -9,22 +10,46 @@ function currentSlide(n){
   showSlides(slideIndex = n)
 }
 
+function playAutoSlide(){
+autoSlideInterval = setInterval(() => plusSlides(1), 3500)
+}
+
+function clearAutoSlide(){
+  clearInterval(autoSlideInterval);
+}
+
+document.querySelector(".play").addEventListener("click", () => {
+clearAutoSlide();
+playAutoSlide();
+})
+
+document.querySelector(".pause").addEventListener("click", () => {
+  clearAutoSlide();
+})
+
+document.addEventListener("keydown", (event) => {
+  if(event.key === "ArrowLeft"){
+    plusSlides(-1)
+  }else if(event.key === "ArrowRight"){
+    plusSlides(1)
+  }
+})
+
 function showSlides(n){
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-
   if(n > slides.length){slideIndex = 1};
   if(n < 1){slideIndex = slides.length};
-
+  
   for(i = 0; i < slides.length; i++){
-    slides[i].style.display = "none";
+  slides[i].style.display = "none"
   }
 
   for(i = 0; i < dots.length; i++){
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].classList.remove("active");
   }
 
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active"
+  dots[slideIndex - 1].classList.add("active");
 }
